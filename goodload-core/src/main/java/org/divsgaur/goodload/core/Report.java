5b521e42d6bcb503ecc28ee5db24071b21f45c7a
Divyansh Shekhar Gaur <divyansh.shekhar@gmail.com>
2021-04-18 15:25:19 +0530

    Made Report class serializable.
    Signed-off-by: Divyansh Shekhar Gaur <divyansh.shekhar@gmail.com>

--- goodload-core/src/main/java/org/divsgaur/goodload/core/Report.java
+++ goodload-core/src/main/java/org/divsgaur/goodload/core/Report.java
@@ -1,8 +1,14 @@
 package org.divsgaur.goodload.core;
 
+import lombok.Data;
+
+import java.io.Serializable;
 import java.util.List;
 
-public class Report {
+@Data
+public class Report implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String stepName;
     private long timeInMillis;
     private List<Report> subSteps;
