e3034672adff3248f0f70e5a386315719e48ae10
Divyansh Shekhar Gaur <divyansh.shekhar@gmail.com>
2021-04-21 19:03:25 +0530

    Added ReportAggregator bean configuration.
    Signed-off-by: Divyansh Shekhar Gaur <divyansh.shekhar@gmail.com>

--- goodload-engine/src/main/java/org/divsgaur/goodload/config/GoodloadConfiguration.java
+++ goodload-engine/src/main/java/org/divsgaur/goodload/config/GoodloadConfiguration.java
@@ -1,6 +1,8 @@
 package org.divsgaur.goodload.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.divsgaur.goodload.reporting.ReportAggregator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +10,13 @@ import org.springframework.context.annotation.Configuration;
 public class GoodloadConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         return new ObjectMapper();
+    }
+
+    @Bean
+    public ReportAggregator reportAggregator() {
+        return new ReportAggregator();
     }
 }
