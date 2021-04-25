5f0b15790ab595c9c8bf828025326df17dc14104
Divyansh Shekhar Gaur <divyanshshekhar@users.noreply.github.com>
2021-04-26 00:47:38 +0530

    Removed parenthesis around single lambda parameter 'session' in SampleSimulation and SampleHttpSimulation
    Signed-off-by: Divyansh Shekhar Gaur <divyanshshekhar@users.noreply.github.com>

--- goodload-sample/src/main/java/org/divsgaur/goodload/samples/SampleSimulation.java
+++ goodload-sample/src/main/java/org/divsgaur/goodload/samples/SampleSimulation.java
@@ -13,18 +13,18 @@ public class SampleSimulation extends Simulation {
     public List<Action> init() {
         Action scenario = scenario("Sample scenario",
                 group("Login",
-                        exec("Login: Exec1", (session) -> {}),
-                        exec("Login: Exec2", (session) -> {}),
+                        exec("Login: Exec1", session -> {}),
+                        exec("Login: Exec2", session -> {}),
                         check(session -> true),
-                        exec("Login: Exec3", (session) -> {})),
-                check((session) -> {
+                        exec("Login: Exec3", session -> {})),
+                check(session -> {
                     return ((String) session.get("HEADER-AUTHENTICATION").orElse("")).equals("401");
                 }),
-                exec("Execution 1: ", (session) -> {String random = "Some random execution";}),
+                exec("Execution 1: ", session -> {String random = "Some random execution";}),
                 group("Logout",
-                        exec("Logout: Exec 1", (session) -> {}),
-                        exec("Logout: Exec 2", (session) -> {}),
-                        exec("Logout: Exec 3", (session) -> {}))
+                        exec("Logout: Exec 1", session -> {}),
+                        exec("Logout: Exec 2", session -> {}),
+                        exec("Logout: Exec 3", session -> {}))
         );
 
         return Collections.singletonList(scenario);
